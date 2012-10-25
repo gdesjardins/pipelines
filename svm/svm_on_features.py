4ccc5cff61f34f07888be58e52e76db4e9836cb8
Guillaume Desjardins <guillaume.desjardins@gmail.com>
2012-10-25 14:52:02 -0400

    BUG FIX: cross-validation was returning model last tested instead of best model. Test error was therefore wrong.

--- svm/svm_on_features.py
+++ svm/svm_on_features.py
@@ -6,26 +6,42 @@ import pickle
 import logging
 from pylearn2.utils import options_parser
 from pylearn2.utils import serial
+from pylearn2.datasets import dense_design_matrix
 from pylearn2.datasets.preprocessing import Standardize
+from pylearn2.training_callbacks.training_callback import TrainingCallback
 from scikits.learn.svm import LinearSVC
+from pylearn2.models import svm
 
 
 logging.basicConfig(level=logging.INFO)
 
+class pylearn2_svm_callback(TrainingCallback):
+
+    def __init__(self, **kwargs):
+        self.svm_on_features = SVMOnFeatures(**kwargs)
+    
+    def __call__(self, model, dataset, algorithm):
+        best_svm, valid_error, test_error = self.svm_on_features.run()
+        i = model.batches_seen
+        if not hasattr(model, 'results'):
+            model.results = {}
+        model.results['batches_seen'] = i
+        model.results['valerr_%i' % i] = valid_error
+        model.results['tsterr_%i' % i] = test_error
+        model.results['C%i' % i] = best_svm.C
+
 
 class SVMOnFeatures():
     
-    def __init__(self, model, svm, trainset, testset, validset=None,
-                 C_list=None, save_fname=None, model_call_kwargs=None,
-                 standardize=False):
+    def __init__(self, svm, trainset, testset,
+                 model=None, model_call_kwargs=None,
+                 validset=None, C_list=None, save_fname=None):
         """
         Performs cross-validation of a linear SVM on features extracted by a unsupervised
         learning module.
 
         Parameters
         ----------
-        model: pylearn2.models.model.Model
-            Unsupervised learning module used for feature extraction.
         svm: sklearn.svm
             Scikit-learn SVM object to use for training.
         trainset: pylearn2.datasets.dataset.Dataset
@@ -33,77 +49,91 @@ class SVMOnFeatures():
         testset: pylearn2.datasets.dataset.Dataset
             Pylearn2 dataset used for final performance measurement. If no validation set is
             provided, the test will also be used for hyper-parameter selection.
+        model: pylearn2.models.model.Model
+            Unsupervised learning module used for feature extraction.
+        model_call_kwargs: dict (optional)
+            Dictionary of arguments to pass to model.call.
         validset: pylearn2.datasets.dataset.Dataset
             Pylearn2 dataset used for hyper-parameter selection.
         C_list: list
             List of penalty parameters to cross-validate for the SVM.
         save_fname: string
             Output filename to store trained svm model.
-        model_call_kwargs: dict (optional)
-            Dictionary of arguments to pass to model.call.
-        standardize: boolean
-            Standardize the model output, before feeding to SVM.
         """
         assert hasattr(model, 'perform')
-        self.model = model
         self.svm = svm
         self.trainset = trainset
         self.validset = validset
         self.testset = testset
+        self.model = model
+        self.model_call_kwargs = model_call_kwargs
         if C_list is None:
             C_list = [1e-3,1e-2,1e-1,1,10]
         self.C_list = C_list
         self.save_fname = save_fname
-        self.model_call_kwargs = model_call_kwargs
 
-    def extract_features(self):
-        postproc = Standardize()
-        self.model.fn = self.model.function("perform", self.model_call_kwargs)
-        # extract new representation for training data
-        self.trainset.X = self.model.perform(self.trainset.X)
-        postproc.apply(self.trainset, can_fit=True)
-        # extract new representation for test data
-        self.testset.X = self.model.perform(self.testset.X)
-        postproc.apply(self.testset, can_fit=False)
-        # extract new representation for validation data
-        if self.validset:
-            self.validset.X = self.model.perform(self.validset.X)
-            postproc.apply(self.validset, can_fit=False)
+    def extract_features(self, dset, preproc=None, can_fit=False):
+        new_dset = dense_design_matrix.DenseDesignMatrix()
+        new_dset.X = self.model.perform(dset.X)
+        new_dset.y = dset.y
+        if preproc:
+            preproc.apply(new_dset, can_fit=True)
+        return new_dset
  
-    def run(self, retrain_on_valid=True):
-        self.extract_features()
-        validset = self.validset if self.validset else self.testset
-        self.svm = cross_validate_svm(self.svm, self.trainset, validset, self.C_list)
+    def run(self, retrain_on_valid=False):
+        # Extract features from model.
+        preproc = Standardize()
+        self.model.fn = self.model.function("perform", **self.model_call_kwargs)
+        newtrain = self.extract_features(self.trainset, preproc, can_fit=True)
+        newtest  = self.extract_features(self.testset, preproc, can_fit=False)
+        newvalid = newtest if not self.validset else\
+                   self.extract_features(self.validset, preproc, can_fit=False)
+        # Find optimal SVM hyper-parameters.
+        (best_svm, valid_error) = cross_validate_svm(self.svm,
+                newtrain, newvalid, self.C_list)
+        logging.info('Best validation error for C=%f : %f' % (best_svm.C, valid_error))
+        # Optionally retrain on validation set, using optimal hyperparams.
         if self.validset and retrain_on_valid:
-            retrain_svm(self.svm, self.trainset, self.validset)
-        test_error = compute_test_error(self.svm, self.testset)
+            retrain_svm(best_svm, newtrain, newvalid)
+        test_error = compute_test_error(best_svm, newtest)
         logging.info('Test error = %f' % test_error)
         if self.save_fname:
             fp = open(self.save_fname, 'w')
-            pickle.dump(self.svm, fp)
+            pickle.dump(best_svm, fp)
             fp.close()
-        return self.svm
+        return (best_svm, valid_error, test_error)
 
 
+import time
 def cross_validate_svm(svm, trainset, validset, C_list):
     best_svm = None
     best_error = numpy.Inf
+    print 'C_list = ', C_list
     for C in C_list:
-        svm.C = C
+        print 'C = ', C
+        t1 = time.time()
+        if hasattr(svm, 'set_params'):
+            svm.set_params(C = C)
+        else:
+            svm.C = C
         svm.fit(trainset.X, trainset.y)
         predy = svm.predict(validset.X)
         error = (validset.y != predy).mean()
         if error < best_error:
             logging.info('SVM(C=%f): valid_error=%f **' % (C, error))
             best_error = error
-            best_svm = copy.copy(svm)
+            best_svm = copy.deepcopy(svm)
+            # Numpy bug workaround: copy module does not respect C/F ordering.
+            best_svm.raw_coef_ = numpy.asarray(best_svm.raw_coef_, order='F')
         else:
             logging.info('SVM(C=%f): valid_error=%f' % (C, error))
-    return best_svm
+        logging.info('Elapsed time: %f' % (time.time() - t1))
+    return (best_svm, best_error)
 
 
 def retrain_svm(svm, trainset, validset):
     assert validset is not None
+    logging.info('Retraining on {train, validation} sets.')
     full_train_X = numpy.vstack((trainset.X, validset.X))
     full_train_y = numpy.hstack((trainset.y, validset.y))
     svm.fit(full_train_X, full_train_y)
@@ -122,4 +152,4 @@ if __name__ == '__main__':
                         help='A YAML configuration file specifying the training procedure')
     args = parser.parse_args()
     obj = serial.load_train_file(args.config)
-    obj.run()
+    obj.run(retrain_on_valid=False)
